9aaf97c136b339c31408d3a5132374dbf864a62d
Tanja Hennis <tmhennis@gmail.com>
2018-05-28 13:44:41 +0200

    add components: App, Header, Inventory, Order

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+  render () {
+    return (
+      <header className="top">
+        <h1>Catch
+          <span className="ofThe">
+            <span className="of">of</span>
+            <span className="the">the</span>
+          </span>
+          Day
+        </h1>
+        <h3 className="tagline">
+          <span>Fresh Daily</span>
+        </h3>
+      </header>
+    )
+  }
+}
