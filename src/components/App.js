457af108b801ee8384cb8fb2ffd65b02b10697d3
Tanja Hennis <tmhennis@gmail.com>
2018-06-18 15:39:02 +0200

    add fishes to order button

--- src/components/App.js
+++ src/components/App.js
@@ -23,13 +23,27 @@ export default class App extends Component {
     this.setState({ fishes: sampleFishes })
   }
 
+  addToOrder = key => {
+    const order = { ...this.state.order }
+
+    order[key] = order[key] + 1 || 1
+
+    this.setState({ order  })
+  }
+
   render () {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="fresh seafood market"/>
           <ul className="fishes">
-            {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]} />)}
+            {Object.keys(this.state.fishes).map(key =>
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder} />
+            )}
           </ul>
         </div>
         <Order />
--- src/components/Fish.js
+++ src/components/Fish.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 import { formatPrice } from '../helpers'
 
 export default class Fish extends Component {
+  handleClick = () => {
+    this.props.addToOrder(this.props.index)
+  }
+
   render() {
     const { image, name, price, desc, status } = this.props.details
     const isAvailable = status === 'available'
@@ -14,7 +18,9 @@ export default class Fish extends Component {
           <span className="price">{formatPrice(price)}</span>
         </h3>
         <p>{desc}</p>
-        <button disabled={!isAvailable}>{isAvailable ? 'Add to Order' : 'Sold Out'}</button>
+        <button disabled={!isAvailable} onClick={this.handleClick}>
+          {isAvailable ? 'Add to Order' : 'Sold Out'}
+        </button>
       </li>
     )
   }
